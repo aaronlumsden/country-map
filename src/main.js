f621ae3c6f832b340632117aca8f6c84a0d8cf3f
Aaron Lumsden <dev@citu.co.uk>
2020-03-06 12:11:27 +0000

    - Added plugins from npm
    - reset-css
    - axios
    - vue-axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import 'reset-css';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
 Vue.config.productionTip = false
 
 new Vue({
