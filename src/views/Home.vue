0ccf83743273a6bdbd660fec71356c020cd789f4
Aaron Lumsden <dev@citu.co.uk>
2020-03-06 12:19:08 +0000

    - Created home page layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
+        
+    <div class="half">
+        ...
+    </div>
     
+    <div class="half">
+       ...
+    </div>
+        
   </div>
 </template>
 
@@ -9,3 +17,32 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style lang="less">
+.home{
+  display:flex;
+  
+  @media(max-width:@mob){
+    display:block;
+  }
+  
+  .half{
+    flex:1;
+    width:50%;
+    
+    &:first-child{
+      background:red;
+    }
+    
+    &:last-child{
+      background:blue;
+    }
+    
+    @media(max-width:@mob){
+      width:100%;
+    }
+    
+  }
+  
+}
+</style>
