a3913b7e06c13e5277476f9a83f7e8f844271b97
Aaron Lumsden <dev@citu.co.uk>
2020-03-06 13:19:32 +0000

    - Set up the click on list item to go to location marker & trigger click to open the pop up

--- src/components/search-list-item.vue
+++ src/components/search-list-item.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <a class="searchItem" href="#">
+  <a class="searchItem" href="#" @click.prevent="goToLocation(location)">
       <span class="name">{{location.name}}</span>
       <span class="capital">Capital: {{location.capital}}</span>
   </a>
@@ -12,6 +12,17 @@ export default {
     location: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    markers: function(){
+      return this.$store.state.markers
+    }
+  },
+  methods: {
+    goToLocation: function(location){
+      // Trigger a click on the marker for the location
+      window.google.maps.event.trigger(this.markers[location.name], 'click')
     }
   }
 }
